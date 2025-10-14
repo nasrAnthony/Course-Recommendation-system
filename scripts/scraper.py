5aebd15654e599e72de6ae827ec0c66cf5c1e55a
nasrAnthony <anthonynasr29@gmail.com>
2025-10-13 22:53:50 -0400

    Created scraper. Created dataset. Updated req file

---
+++ scripts/scraper.py
@@ -0,0 +1,77 @@
+import os
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+#Statics
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_FP = os.path.join(BASE_DIR, 'data', 'data.csv')
+
+#Testing on elg catalog only
+target_paths = ['https://catalogue.uottawa.ca/en/courses/elg/', #electrical
+                'https://catalogue.uottawa.ca/en/courses/ceg/', #computer
+                'https://catalogue.uottawa.ca/en/courses/bmg/', #biomedical
+                'https://catalogue.uottawa.ca/en/courses/chg/', #chemical
+                'https://catalogue.uottawa.ca/en/courses/seg/', #software
+                'https://catalogue.uottawa.ca/en/courses/cvg/', #civil
+                'https://catalogue.uottawa.ca/en/courses/mcg/', #mechanical
+                ]
+
+
+#helper
+def is_french(course_code: str) -> bool:
+    """
+    if french -> True
+    if not french -> False
+
+    Checking via 2nd course digit (5, 7, 9 -> French)
+    """
+    french_codes = ['5', '7', '9']
+    return(course_code[1] in french_codes)
+
+def scraper():
+    """
+    Main scraper unit
+    """
+    count = 0
+    with open(DATA_FP, "w", newline="", encoding="utf-8") as data_file:
+        writer = csv.writer(data_file)
+        writer.writerow(["Faculty", "Code", "Title", "Description"])
+
+        for url in target_paths:
+            print(f"Starting scrape on -> {url}")
+            try:
+                response = requests.get(url)
+                response.raise_for_status()
+            except requests.exceptions.RequestException as e:
+                print(f"Error fetching {url}: {e}")
+                continue
+            soup = BeautifulSoup(response.text, 'html.parser')
+            course_blocks = soup.find_all("div", class_="courseblock")
+            for block in course_blocks:
+                title_tag = block.find("h3", class_="courseblocktitle noindent")
+
+                if not title_tag:
+                    continue
+
+                title_text = title_tag.get_text(strip=True)
+                gen_info = title_text.split(" ", 1)
+                course_faculty, course_code = gen_info[0][0:3], gen_info[0][-4:]
+
+                if is_french(course_code=course_code):
+                    continue
+
+                course_title = gen_info[-1].split('(3')[0]
+                desc_tag = block.find("p", class_="courseblockdesc noindent")
+
+                if not desc_tag:
+                    continue
+
+                course_description = desc_tag.get_text(strip=True)
+
+                writer.writerow([course_faculty, course_code, course_title, course_description])
+                count += 1
+        print(f"Successfully scraped a total of {count} courses.")
+
+if __name__ == "__main__":
+    scraper()
