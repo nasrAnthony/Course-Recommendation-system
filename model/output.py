23dd4515f515a817077e5a95f32307ce70c95f1c
Yusuf Hilal <yusufyh02@gmail.com>
2025-11-29 03:31:46 -0500

    Added multiple student evaluation and metric adjustment

--- model/output.py
+++ model/output.py
@@ -1,3 +1,4 @@
+import re
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -40,14 +41,19 @@ def print_recommendations(recs_df):
     
     
 def metrics(top_df, liked_courses_str):
-    # @Yhilal02 look at this again and clean it up
-
-    liked_list = [x.strip() for x in liked_courses_str.split(";") if x.strip()]
+    ''' 
+    takes in the top n recommendation dataframe and liked course string
+    returns metrics for the student
+    '''
+    # format liked courses
+    tokens = re.split(r"[;,]", liked_courses_str)
+    liked_list = [x.strip() for x in tokens if x.strip()]
     liked_set = set(liked_list)
 
     matches = []
     first_match_rank = None
 
+    # checking the recieved courses vs liked courses
     for rank, (_, row) in enumerate(top_df.iterrows(), start=1):
         fac_code = f"{row['Faculty']} {row['Code']}"
         
@@ -57,22 +63,18 @@ def metrics(top_df, liked_courses_str):
             if first_match_rank is None:
                 first_match_rank = rank
 
-    top_n = len(top_df)
+    n = len(top_df)
     num_matches = len(matches)
-    num_liked = len(liked_list)
-    
+        
     # metrics calculations
-    hit = 1 if num_matches > 0 else 0
-    precision = num_matches / top_n if top_n > 0 else 0
-    recall = num_matches / num_liked if num_liked > 0 else 0
-    mrr = 1 / first_match_rank if first_match_rank is not None else 0
+    hit = 1 if num_matches > 0 else 0                       # was one of the liked courses recommended
+    precision = num_matches / n if n > 0 else 0             # @Yhilal02 revisit this vs recall and see which is better (f1 score? scaling up precision?)
+    mrr = 1 / first_match_rank if first_match_rank else 0   # how early was match received
 
     return {
-        "matches": matches,
         "hit": hit,
         "precision": precision,
-        "recall": recall,
-        "mrr": mrr,
+        "mrr": mrr
     }
     
     
@@ -99,3 +101,28 @@ def cosine_stats_and_plot(emb, label):
     plt.ylabel("Count")
     plt.grid(True)
     plt.show()
+    
+    
+def evaluate_many_students(student_embs, course_embs, course_df, student_df, top_n=5):
+    ''' gets the metrics for multiple students'''
+    results = []
+
+    for i in range(len(student_df)):
+        # get the top n for student[i]
+        student_emb = student_embs[i]
+        liked_str = student_df.loc[i, "LikedCourses"]
+
+        top_df = get_top_n_recommendations(
+            student_emb,
+            course_embs,
+            course_df,
+            top_n
+        )
+
+        # save their metrics
+        m = metrics(top_df, liked_str)
+        m["student_index"] = i
+        m["num_liked"] = len([x.strip() for x in re.split(r"[;,]", liked_str) if x.strip()])
+        results.append(m)
+
+    return pd.DataFrame(results)
